94fadbd30637887d13f624f544205f43089e9328
ismail1k <oneigcy@gmail.com>
2022-03-27 18:55:43 +0000

    Commit

---
+++ src/CartServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Ismail1k\LaravelApiCart;
+
+use Illuminate\Support\ServiceProvider;
+
+class CartServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->publishes([
+            __DIR__.'/../database/migrations/' => database_path('migrations'),
+        ]);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
