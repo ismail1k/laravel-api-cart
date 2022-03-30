e00a60de861246d2f6d3cace9313c9d04cfc943e
Ismail andaloussi <oneigcy@gmail.com>
2022-03-30 22:45:23 +0000

    Commit

--- src/Facades/Cart.php
+++ src/Facades/Cart.php
@@ -13,19 +13,20 @@ class Cart
                 'total' => $total,
                 'items' => [],
             ];
-            $cart = DB::table('carts')->where('cart_id', $cart_id)->first();
-            $items = DB::table('cart-items')->where('cart_id'. $cart->id)->all();
-            foreach($items as $item){
-                $product = DB::table('products')->where('product_id', $item->product_id)->first();
-                array_push($prototype['items'], [
-                    'item_id' => $item->id,
-                    'id' => $product->id,
-                    'name' => $product->name,
-                    'description' => $product->short_description,
-                    'inventory_id' => $product->inventory_id,
-                    'unit_price' => $product->price-$product->discount,
-                    'quantity' => $item->quantity,
-                ]);
+            if($cart = DB::table('carts')->where('cart_id', $cart_id)->first()){
+                $items = DB::table('cart-items')->where('cart_id'. $cart->id)->all();
+                foreach($items as $item){
+                    $product = DB::table('products')->where('product_id', $item->product_id)->first();
+                    array_push($prototype['items'], [
+                        'item_id' => $item->id,
+                        'id' => $product->id,
+                        'name' => $product->name,
+                        'description' => $product->short_description,
+                        'inventory_id' => $product->inventory_id,
+                        'unit_price' => $product->price-$product->discount,
+                        'quantity' => $item->quantity,
+                    ]);
+                }
             }
             return (object)$prototype;
         }
